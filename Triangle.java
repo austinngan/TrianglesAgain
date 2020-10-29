ba0fcfa96f3773ba812f1d62a30024c5ce94d9f8
austinngan <angan20@stuy.edu>
2020-10-29 12:17:55 -0400

    Added Triangle class

---
+++ Triangle.java
@@ -0,0 +1,11 @@
+public class Triangle{
+  private Point v1, v2, v3;
+  public Triangle(Point a, Point b, Point c){
+    v1=a;
+    v2=b;
+    v3=c;
+  }
+  public Triangle(double x1, double y1,double x2, double y2,double x3, double y3){
+
+  }
+}

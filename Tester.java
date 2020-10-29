29431e4f2f9f547c7737a0303bda979839bbaa36
austinngan <angan20@stuy.edu>
2020-10-29 12:27:38 -0400

    Added test case for getPerimeter

--- Tester.java
+++ Tester.java
@@ -1,11 +1,14 @@
 public class Tester{
   public static void main(String[]args){
-    Point a=new Point(2.0, 0.0);
-    Point b=new Point(1.0,0.0);
-    Point c=new Point(2.0,0.0);
+    Point a=new Point(0.5248, -0.8002);
+    Point b=new Point(-2.437885, 1.94);
+    Point c=new Point(0.5248, -0.8002);
+    Point d=new Point(0.0,0.0);
+    Triangle s1=new Triangle(a,b,d);
     System.out.println(a.getX());
     System.out.println(a.getY());
     System.out.println(a.distanceTo(b));
     System.out.println(a.equals(c));
+    System.out.println(s1.getPerimeter());
   }
 }

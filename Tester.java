f78ade68df254f4e7beb0276bc1e55786c9bcfea
austinngan <angan20@stuy.edu>
2020-10-29 12:10:43 -0400

    Added test case for equals method

--- Tester.java
+++ Tester.java
@@ -2,8 +2,10 @@ public class Tester{
   public static void main(String[]args){
     Point a=new Point(2.0, 0.0);
     Point b=new Point(1.0,0.0);
+    Point c=new Point(2.0,0.0);
     System.out.println(a.getX());
     System.out.println(a.getY());
     System.out.println(a.distanceTo(b));
+    System.out.println(a.equals(c));
   }
 }

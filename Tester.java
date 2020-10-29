28d44cf63ea4d54b0caa8e8618ec0ece9841073a
austinngan <angan20@stuy.edu>
2020-10-29 12:08:19 -0400

    Added test case for distance method

--- Tester.java
+++ Tester.java
@@ -1,5 +1,9 @@
 public class Tester{
   public static void main(String[]args){
-    Point a=new Point(4, 15);
+    Point a=new Point(2.0, 0.0);
+    Point b=new Point(1.0,0.0);
+    System.out.println(a.getX());
+    System.out.println(a.getY());
+    System.out.println(a.distanceTo(b));
   }
 }
